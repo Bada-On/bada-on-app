69bbc48bb39947b8ca3ae1552ca4bc610ea1f5c5
hanbeulYou <hanbeul.you@gmail.com>
2024-11-25 01:32:12 +0900

    feat: WebView 추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Alert, View, Text } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [webkey, setWebkey] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        style={styles.webview}
+        source={{ uri: "https://bada-on-fe.vercel.app/" }}
+        onError={(event) => {
+          console.error("WebView error:", event.nativeEvent);
+          Alert.alert("오류 발생", "웹뷰 로딩 중 오류가 발생했습니다!");
+        }}
+        onHttpError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.error("HTTP error:", nativeEvent);
+          Alert.alert("오류 발생", "웹뷰 로딩 중 오류가 발생했습니다!");
+        }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        originWhitelist={["*"]}
+        scalesPageToFit={true}
+        mixedContentMode="compatibility"
+        key={webkey}
+        onLoadStart={(e) => {
+          const { nativeEvent } = e;
+          if (nativeEvent.url === "about:blank" && !ready) {
+            setWebkey(Date.now());
+          }
+        }}
+        onLoadEnd={() => {
+          if (!ready) {
+            setWebkey(Date.now());
+            setReady(true);
+          }
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  webview: {
+    flex: 1,
   },
 });
